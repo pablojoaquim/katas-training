94d7cccfb472e4318a5a8459316963c1e18d0534
pablo.joaquim <pablo.joaquim@gmail.com>
2026-01-13 16:09:33 -0300

    Playing with semaphores

--- src/main.cpp
+++ src/main.cpp
@@ -41,11 +41,25 @@
 #include <signal.h>
 #include <fcntl.h>
 #include <mqueue.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
 
 /*===========================================================================*
  * Local Preprocessor #define Constants
  *===========================================================================*/
 #define NDEBUG
+
+#define MAX_RETRIES 10
+
+// #ifdef NEED_SEMUN
+/* Defined in sys/sem.h as required by POSIX now */
+union semun
+{
+    int val;
+    struct semid_ds *buf;
+    ushort *array;
+};
+// #endif
 
 /*===========================================================================*
  * Local Preprocessor #define MACROS
@@ -79,25 +93,69 @@ extern "C"
 {
 }
 
-/**
- * Input a priority and message from the keyboard.
- *
- * Really fragile--for demo purposes only.
- */
-int user_input(char *buf, size_t bufsize, unsigned int *msg_prio)
+/*
+** initsem() -- more-than-inspired by W. Richard Stevens' UNIX Network
+** Programming 2nd edition, volume 2, lockvsem.c, page 295.
+*/
+int initsem(key_t key, int nsems) /* key from ftok() */
 {
-    printf("Priority and message (e.g. 2 hi): ");
-    fflush(stdout);
-    fgets(buf, bufsize - 1, stdin);
-    buf[bufsize - 1] = '\0';
-    char *token = strtok(buf, " \n");
-    if (token == NULL)
-        return 0;
-    *msg_prio = atoi(token); // Get priority
-    token = strtok(NULL, "\n");
-    int msg_len = strlen(token) + 1;
-    memmove(buf, token, msg_len);
-    return msg_len;
+    int i;
+    union semun arg;
+    struct semid_ds buf;
+    struct sembuf sb;
+    int semid;
+    semid = semget(key, nsems, IPC_CREAT | IPC_EXCL | 0666);
+    if (semid >= 0)
+    { /* we got it first */
+        sb.sem_op = 1;
+        sb.sem_flg = 0;
+        arg.val = 1;
+        printf("press return\n");
+        getchar();
+        for (sb.sem_num = 0; sb.sem_num < nsems; sb.sem_num++)
+        {
+            /* do a semop() to "free" the semaphores. */
+            /* this sets the sem_otime field, as needed below. */
+            if (semop(semid, &sb, 1) == -1)
+            {
+                int e = errno;
+                semctl(semid, 0, IPC_RMID); /* clean up */
+                errno = e;
+                return -1; /* error, check errno */
+            }
+        }
+    }
+    else if (errno == EEXIST)
+    { /* someone else got it first */
+        int ready = 0;
+        semid = semget(key, nsems, 0); /* get the id */
+        if (semid < 0)
+            return semid; /* error, check errno */
+        /* wait for other process to initialize the semaphore: */
+        arg.buf = &buf;
+        for (i = 0; i < MAX_RETRIES && !ready; i++)
+        {
+            semctl(semid, nsems - 1, IPC_STAT, arg);
+            if (arg.buf->sem_otime != 0)
+            {
+                ready = 1;
+            }
+            else
+            {
+                sleep(1);
+            }
+        }
+        if (!ready)
+        {
+            errno = ETIME;
+            return -1;
+        }
+    }
+    else
+    {
+        return semid; /* error, check errno */
+    }
+    return semid;
 }
 
 /*****************************************************************************
@@ -110,48 +168,68 @@ int main(int argc, char *argv[])
 {
     std::cout << "=== Start ===" << std::endl;
 
-    if (!strcmp(argv[1], "sender"))
+    if (!strcmp(argv[1], "seminit"))
     {
-        char msg[128];
-        mqd_t mqdes = mq_open("/mq_test", O_WRONLY | O_CREAT, 0644);
-        for (;;)
+        key_t key;
+        int semid;
+        struct sembuf sb;
+        sb.sem_num = 0;
+        sb.sem_op = -1; /* set to allocate resource */
+        sb.sem_flg = SEM_UNDO;
+        if ((key = 123) == -1)
         {
-            unsigned int msg_prio;
-            int msg_len = user_input(msg, sizeof msg, &msg_prio);
-            if (msg_len == 0)
-                break;
-            printf("sending \"%s\" (%d bytes) at priority %u\n", msg, msg_len, msg_prio);
-            if (mq_send(mqdes, msg, msg_len, msg_prio) == -1)
-            {
-                perror("mq_send");
-            }
+            perror("ftok");
+            exit(1);
         }
-        mq_close(mqdes);
-        mq_unlink("/mq_test");
+        /* grab the semaphore set created by seminit: */
+        if ((semid = initsem(key, 1)) == -1)
+        {
+            perror("initsem");
+            exit(1);
+        }
+        printf("Press return to lock: ");
+        getchar();
+        printf("Trying to lock...\n");
+        if (semop(semid, &sb, 1) == -1)
+        {
+            perror("semop");
+            exit(1);
+        }
+        printf("Locked.\n");
+        printf("Press return to unlock: ");
+        getchar();
+        sb.sem_op = 1; /* free resource */
+        if (semop(semid, &sb, 1) == -1)
+        {
+            perror("semop");
+            exit(1);
+        }
+        printf("Unlocked\n");
+        return 0;
     }
     else
     {
-        mqd_t mqdes = mq_open("/mq_test", O_RDONLY);
-        char msg[128];
-        char msg_len = sizeof msg;
-        unsigned int msg_prio;
-        ssize_t bytes_received;
-        for (;;)
+        key_t key;
+        int semid;
+        union semun arg;
+        if ((key = 123) == -1)
         {
-            bytes_received = mq_receive(mqdes, msg, msg_len, &msg_prio);
-            if (bytes_received == -1)
-            {
-                perror("mq_receive");
-                return 1;
-            }
-            printf("received \"%s\" (%zd bytes) at priority %u\n", msg, bytes_received, msg_prio);
-
-            // To finalize the receiver
-            if(msg_prio == 0)
-                break;
+            perror("ftok");
+            exit(1);
         }
-        mq_close(mqdes);
-        mq_unlink("/mq_test");
+        /* grab the semaphore set created by seminit: */
+        if ((semid = semget(key, 1, 0)) == -1)
+        {
+            perror("semget");
+            exit(1);
+        }
+        /* remove it: */
+        if (semctl(semid, 0, IPC_RMID, 0) == -1)
+        {
+            perror("semctl");
+            exit(1);
+        }
+        return 0;
     }
 
     std::cout << "===  End  ===" << std::endl;
