fd2e405807561275278497f16476fdffbef39957
pablo.joaquim <pablo.joaquim@gmail.com>
2026-01-13 15:30:05 -0300

    message queue IPC tested. The /build/main program could be called with either parameter 'sender' or 'receiver' to test both actors

--- src/main.cpp
+++ src/main.cpp
@@ -32,12 +32,15 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <cstring>
 #include <cstdint>
 #include "katas.h"
 #include "parser.h"
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <fcntl.h>
+#include <mqueue.h>
 
 /*===========================================================================*
  * Local Preprocessor #define Constants
@@ -60,8 +63,6 @@
 /*===========================================================================*
  * Local Variables Definitions
  *===========================================================================*/
-volatile sig_atomic_t got_usr1;
-volatile sig_atomic_t got_int;
 
 /*===========================================================================*
  * Local Function Prototypes
@@ -78,20 +79,25 @@ extern "C"
 {
 }
 
-void sigusr1_handler(int sig)
+/**
+ * Input a priority and message from the keyboard.
+ *
+ * Really fragile--for demo purposes only.
+ */
+int user_input(char *buf, size_t bufsize, unsigned int *msg_prio)
 {
-    got_usr1 = 1;
-    /* using a char[] so that sizeof will work */
-    const char msg[] = "SIGUSR1 signal received!\n";
-    write(0, msg, sizeof(msg));
-}
-
-void sigint_handler(int sig)
-{
-    got_int = 1;
-    /* using a char[] so that sizeof will work */
-    const char msg[] = "SIGINT signal received!\n";
-    write(0, msg, sizeof(msg));
+    printf("Priority and message (e.g. 2 hi): ");
+    fflush(stdout);
+    fgets(buf, bufsize - 1, stdin);
+    buf[bufsize - 1] = '\0';
+    char *token = strtok(buf, " \n");
+    if (token == NULL)
+        return 0;
+    *msg_prio = atoi(token); // Get priority
+    token = strtok(NULL, "\n");
+    int msg_len = strlen(token) + 1;
+    memmove(buf, token, msg_len);
+    return msg_len;
 }
 
 /*****************************************************************************
@@ -102,53 +108,49 @@ void sigint_handler(int sig)
  *****************************************************************************/
 int main(int argc, char *argv[])
 {
-    int pfds[2];
-    char buf[30];
-
     std::cout << "=== Start ===" << std::endl;
 
-    if (pipe(pfds) == -1)
+    if (!strcmp(argv[1], "sender"))
     {
-        perror("pipe");
-        exit(1);
+        char msg[128];
+        mqd_t mqdes = mq_open("/mq_test", O_WRONLY | O_CREAT, 0644);
+        for (;;)
+        {
+            unsigned int msg_prio;
+            int msg_len = user_input(msg, sizeof msg, &msg_prio);
+            if (msg_len == 0)
+                break;
+            printf("sending \"%s\" (%d bytes) at priority %u\n", msg, msg_len, msg_prio);
+            if (mq_send(mqdes, msg, msg_len, msg_prio) == -1)
+            {
+                perror("mq_send");
+            }
+        }
+        mq_close(mqdes);
     }
-
-    pid_t pid;
-    switch (pid = fork())
+    else
     {
-    case -1:
-        perror("fork"); /* something went wrong */
-        exit(1);        /* parent exits */
-    case 0:
-        printf(" CHILD: This is the child process!\n");
-        printf(" CHILD: My PID is %d\n", getpid());
-        printf(" CHILD: My parent's PID is %d\n", getppid());
-        printf(" CHILD: writing to the pipe\n");
-        write(pfds[1], "test", 5);
-        printf(" CHILD: Terminating!\n");
-        exit(0);
-    default:
-        printf("PARENT: This is the parent process!\n");
-        printf("PARENT: My PID is %d\n", getpid());
-        printf("PARENT: My child's PID is %d\n", pid);
-        read(pfds[0], buf, 5);
-        printf("PARENT: read \"%s\"\n", buf);
-        wait(NULL);
-        printf("PARENT: Terminating!\n");
+        mqd_t mqdes = mq_open("/mq_test", O_RDONLY);
+        char msg[128];
+        char msg_len = sizeof msg;
+        unsigned int msg_prio;
+        ssize_t bytes_received;
+        for (;;)
+        {
+            bytes_received = mq_receive(mqdes, msg, msg_len, &msg_prio);
+            if (bytes_received == -1)
+            {
+                perror("mq_receive");
+                return 1;
+            }
+            printf("received \"%s\" (%zd bytes) at priority %u\n", msg, bytes_received, msg_prio);
+
+            // To finalize the receiver
+            if(msg_prio == 0)
+                break;
+        }
+        mq_close(mqdes);
     }
-
-    // const int arr1[] = {1, 2,3,4,5,6,6,7,8,9};
-    // const int arr2[] = {1};
-
-    // size_t length;
-    // int *actual = camelCaseBreaker(arr1, ARRAY_LENGTH(arr1), arr2, ARRAY_LENGTH(arr2), &length);
-
-    // printf("{ ");
-    // for (size_t i = 0; i < length; i++)
-    //     printf("%d%s", actual[i], (i == length - 1) ? "" : ", ");
-    // printf(" }");
-
-    // free(actual);
 
     std::cout << "===  End  ===" << std::endl;
     return 0;
