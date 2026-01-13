fd10b5e1affb158445344889421f979dfebf59f8
pablo.joaquim <pablo.joaquim@gmail.com>
2026-01-13 16:21:57 -0300

    Adding some easy example of a shared memory

--- src/main.cpp
+++ src/main.cpp
@@ -43,23 +43,14 @@
 #include <mqueue.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
+#include <sys/shm.h>
 
 /*===========================================================================*
  * Local Preprocessor #define Constants
  *===========================================================================*/
 #define NDEBUG
 
-#define MAX_RETRIES 10
-
-// #ifdef NEED_SEMUN
-/* Defined in sys/sem.h as required by POSIX now */
-union semun
-{
-    int val;
-    struct semid_ds *buf;
-    ushort *array;
-};
-// #endif
+#define SHM_SIZE 1024 /* make it a 1K shared memory segment */
 
 /*===========================================================================*
  * Local Preprocessor #define MACROS
@@ -93,71 +84,6 @@ extern "C"
 {
 }
 
-/*
-** initsem() -- more-than-inspired by W. Richard Stevens' UNIX Network
-** Programming 2nd edition, volume 2, lockvsem.c, page 295.
-*/
-int initsem(key_t key, int nsems) /* key from ftok() */
-{
-    int i;
-    union semun arg;
-    struct semid_ds buf;
-    struct sembuf sb;
-    int semid;
-    semid = semget(key, nsems, IPC_CREAT | IPC_EXCL | 0666);
-    if (semid >= 0)
-    { /* we got it first */
-        sb.sem_op = 1;
-        sb.sem_flg = 0;
-        arg.val = 1;
-        printf("press return\n");
-        getchar();
-        for (sb.sem_num = 0; sb.sem_num < nsems; sb.sem_num++)
-        {
-            /* do a semop() to "free" the semaphores. */
-            /* this sets the sem_otime field, as needed below. */
-            if (semop(semid, &sb, 1) == -1)
-            {
-                int e = errno;
-                semctl(semid, 0, IPC_RMID); /* clean up */
-                errno = e;
-                return -1; /* error, check errno */
-            }
-        }
-    }
-    else if (errno == EEXIST)
-    { /* someone else got it first */
-        int ready = 0;
-        semid = semget(key, nsems, 0); /* get the id */
-        if (semid < 0)
-            return semid; /* error, check errno */
-        /* wait for other process to initialize the semaphore: */
-        arg.buf = &buf;
-        for (i = 0; i < MAX_RETRIES && !ready; i++)
-        {
-            semctl(semid, nsems - 1, IPC_STAT, arg);
-            if (arg.buf->sem_otime != 0)
-            {
-                ready = 1;
-            }
-            else
-            {
-                sleep(1);
-            }
-        }
-        if (!ready)
-        {
-            errno = ETIME;
-            return -1;
-        }
-    }
-    else
-    {
-        return semid; /* error, check errno */
-    }
-    return semid;
-}
-
 /*****************************************************************************
  * @fn         main
  * @brief      The main entry point
@@ -168,68 +94,50 @@ int main(int argc, char *argv[])
 {
     std::cout << "=== Start ===" << std::endl;
 
-    if (!strcmp(argv[1], "seminit"))
+    key_t key;
+    int shmid;
+    char *data;
+    int mode;
+    if (argc > 2)
     {
-        key_t key;
-        int semid;
-        struct sembuf sb;
-        sb.sem_num = 0;
-        sb.sem_op = -1; /* set to allocate resource */
-        sb.sem_flg = SEM_UNDO;
-        if ((key = 123) == -1)
-        {
-            perror("ftok");
-            exit(1);
-        }
-        /* grab the semaphore set created by seminit: */
-        if ((semid = initsem(key, 1)) == -1)
-        {
-            perror("initsem");
-            exit(1);
-        }
-        printf("Press return to lock: ");
-        getchar();
-        printf("Trying to lock...\n");
-        if (semop(semid, &sb, 1) == -1)
-        {
-            perror("semop");
-            exit(1);
-        }
-        printf("Locked.\n");
-        printf("Press return to unlock: ");
-        getchar();
-        sb.sem_op = 1; /* free resource */
-        if (semop(semid, &sb, 1) == -1)
-        {
-            perror("semop");
-            exit(1);
-        }
-        printf("Unlocked\n");
-        return 0;
+        fprintf(stderr, "usage: main [data_to_write]\n");
+        exit(1);
+    }
+    /* make the key: */
+    if ((key = 123) == -1)
+    {
+        perror("ftok");
+        exit(1);
+    }
+    /* connect to (and possibly create) the segment: */
+    if ((shmid = shmget(key, SHM_SIZE, 0644 | IPC_CREAT)) == -1)
+    {
+        perror("shmget");
+        exit(1);
+    }
+    /* attach to the segment to get a pointer to it: */
+    data = (char*)shmat(shmid, (void *)0, 0);
+    /* we _could_ use MAP_FAILED, but technically that's not */
+    /* the defined return value. System V failed on this one! */
+    if (data == (void *)(-1))
+    {
+        perror("shmat");
+        exit(1);
+    }
+    /* read or modify the segment, based on the command line: */
+    if (argc == 2)
+    {
+        printf("writing to segment: \"%s\"\n", argv[1]);
+        strncpy(data, argv[1], SHM_SIZE);
+        data[SHM_SIZE - 1] = '\0';
     }
     else
+        printf("segment contains: \"%s\"\n", data);
+    /* detach from the segment: */
+    if (shmdt(data) == -1)
     {
-        key_t key;
-        int semid;
-        union semun arg;
-        if ((key = 123) == -1)
-        {
-            perror("ftok");
-            exit(1);
-        }
-        /* grab the semaphore set created by seminit: */
-        if ((semid = semget(key, 1, 0)) == -1)
-        {
-            perror("semget");
-            exit(1);
-        }
-        /* remove it: */
-        if (semctl(semid, 0, IPC_RMID, 0) == -1)
-        {
-            perror("semctl");
-            exit(1);
-        }
-        return 0;
+        perror("shmdt");
+        exit(1);
     }
 
     std::cout << "===  End  ===" << std::endl;
