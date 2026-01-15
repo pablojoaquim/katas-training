cf93a3be5fbfbba80ff0cb79324c4df0a52de647
pablo.joaquim <pablo.joaquim@gmail.com>
2026-01-15 16:39:01 -0300

    Kata https://www.codewars.com/kata/5526fc09a1bbd946250002dc solved

--- src/main.cpp
+++ src/main.cpp
@@ -36,16 +36,6 @@
 #include <cstdint>
 #include "katas.h"
 #include "parser.h"
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <signal.h>
-#include <fcntl.h>
-#include <mqueue.h>
-#include <sys/ipc.h>
-#include <sys/sem.h>
-#include <sys/shm.h>
-#include <sys/socket.h>
-#include <sys/un.h>
 
 /*===========================================================================*
  * Local Preprocessor #define Constants
@@ -96,85 +86,8 @@ int main(int argc, char *argv[])
 {
     std::cout << "=== Start ===" << std::endl;
 
-    if (!strcmp(argv[1], "server"))
-    {
-        int server_fd = socket(AF_UNIX, SOCK_STREAM, 0);
-        if (server_fd < 0)
-        {
-            perror("socket");
-            return 1;
-        }
-
-        sockaddr_un addr{};
-        addr.sun_family = AF_UNIX;
-        std::strcpy(addr.sun_path, SOCK_PATH);
-
-        unlink(SOCK_PATH);
-
-        if (bind(server_fd, (sockaddr *)&addr, sizeof(addr)) < 0)
-        {
-            perror("bind");
-            return 1;
-        }
-
-        if (listen(server_fd, 1) < 0)
-        {
-            perror("listen");
-            return 1;
-        }
-
-        printf("Server listening on %s\n", SOCK_PATH);
-
-        int client_fd = accept(server_fd, nullptr, nullptr);
-        if (client_fd < 0)
-        {
-            perror("accept");
-            return 1;
-        }
-
-        char buf[128];
-        int n = read(client_fd, buf, sizeof(buf));
-        if (n > 0)
-        {
-            write(client_fd, buf, n); // echo
-        }
-
-        close(client_fd);
-        close(server_fd);
-        unlink(SOCK_PATH);
-    }
-    else
-    {
-        int fd = socket(AF_UNIX, SOCK_STREAM, 0);
-        if (fd < 0)
-        {
-            perror("socket");
-            return 1;
-        }
-
-        sockaddr_un addr{};
-        addr.sun_family = AF_UNIX;
-        std::strcpy(addr.sun_path, SOCK_PATH);
-
-        if (connect(fd, (sockaddr *)&addr, sizeof(addr)) < 0)
-        {
-            perror("connect");
-            return 1;
-        }
-
-        const char *msg = "hola WSL\n";
-        write(fd, msg, strlen(msg));
-
-        char buf[128];
-        int n = read(fd, buf, sizeof(buf) - 1);
-        if (n > 0)
-        {
-            buf[n] = '\0';
-            printf("echo: %s", buf);
-        }
-
-        close(fd);
-    }
+    const int array[] = {160, 3, 1719, 19, 11, 13, -21};
+    find_outlier(array, NumElems(array));
 
     std::cout << "===  End  ===" << std::endl;
     return 0;
