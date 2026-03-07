c5bd18f5c168eb8c153e93a290746d5953836f82
Pablo Joaquim <pablojoaquim@gmail.com>
2026-03-06 23:46:02 -0300

    Developing a mini spotify application

--- src/server.cpp
+++ src/server.cpp
@@ -45,10 +45,43 @@ void http_server_example(void)
 {
     httplib::Server svr;
 
-    svr.Get("/hi", [](const httplib::Request &, httplib::Response &res)
-            { res.set_content("Hello World!", "text/plain"); });
+    // Página principal
+    svr.Get("/", [](const httplib::Request &, httplib::Response &res)
+            {
+        std::string html = R"(
+        <html>
+        <body style="font-family:sans-serif">
+            <h1>Mini Spotify</h1>
+            <p>Play song:</p>
 
-    svr.listen("localhost", 8080);
+            <audio controls>
+                <source src="/song" type="audio/mpeg">
+            </audio>
+
+        </body>
+        </html>
+        )";
+        res.set_content(html, "text/html"); });
+
+    svr.Get("/song", [](const httplib::Request &, httplib::Response &res)
+            {
+        std::ifstream file("music/Genesis.mp3", std::ios::binary);
+
+        if (!file) 
+        {
+            std::cout << "ERROR: song.mp3 not found!" << std::endl;
+            res.status = 404;
+            return;
+        }
+
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+
+        res.set_content(buffer.str(), "audio/mpeg"); });
+
+    std::cout << "Mini Spotify running at http://localhost:8080\n";
+
+    svr.listen("0.0.0.0", 8080);
 }
 
 /*****************************************************************************
