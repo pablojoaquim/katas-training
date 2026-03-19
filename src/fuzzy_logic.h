6baca9a1363767aab6c92e85e07350d5968de449
pablo.joaquim <pablo.joaquim@gmail.com>
2026-03-19 11:59:16 -0300

    Starting to develop a fuzzy logic module

---
+++ src/fuzzy_logic.h
@@ -0,0 +1,86 @@
+#ifndef FUZZY_LOGIC_H
+#define FUZZY_LOGIC_H
+
+/*===========================================================================*/
+/**
+ * @file fuzzy_logic.h
+ *
+ *------------------------------------------------------------------------------
+ * Copyright (c) 2026 - Pablo Joaquim
+ * MIT License: https://opensource.org/licenses/MIT
+ *------------------------------------------------------------------------------
+ *
+ * @section DESC DESCRIPTION: API for the Fuzzy Logic module
+ *
+ * @todo Add full description here
+ *
+ * @section ABBR ABBREVIATIONS:
+ *   - @todo List any abbreviations, precede each with a dash ('-').
+ *
+ * @section TRACE TRACEABILITY INFO:
+ *   - Design Document(s):
+ *     - @todo Update list of design document(s).
+ *
+ *   - Requirements Document(s):
+ *     - @todo Update list of requirements document(s)
+ *
+ *   - Applicable Standards (in order of precedence: highest first):
+ *     - @todo Update list of other applicable standards
+ *
+ */
+/*==========================================================================*/
+
+/*===========================================================================*
+ * Header Files (Common to C and C++)
+ *===========================================================================*/
+
+#ifdef __cplusplus
+/*===========================================================================*
+ * Header Files (C++ only)
+ *===========================================================================*/
+#include <cstdint>
+#endif
+
+/*===========================================================================*
+ * Exported Preprocessor #define Constants
+ *===========================================================================*/
+
+/*===========================================================================*
+ * Exported Preprocessor #define MACROS
+ *===========================================================================*/
+
+/*===========================================================================*
+ * Exported Type Declarations
+ *===========================================================================*/
+
+/*===========================================================================*
+ * Exported Classes (C++ only)
+ *===========================================================================*/
+#ifdef __cplusplus
+
+#endif
+
+/*===========================================================================*
+ * Exported C Function Prototypes
+ *===========================================================================*/
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    // @todo: Add pure C function prototypes here.
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+/*===========================================================================*
+ * Exported C++ Function Prototypes
+ *===========================================================================*/
+#ifdef __cplusplus
+// @todo: Add pure C++ function prototypes here.
+
+#endif /* __cplusplus */
+
+/*===========================================================================*/
+/*===========================================================================*/
+#endif /* FUZZY_LOGIC_H */
